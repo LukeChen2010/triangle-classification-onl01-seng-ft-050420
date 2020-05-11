60f36ae66c6b8ddb6350d630d013a9ccc4b5e2cc
Luke Chen <luke.chen.2010@gmail.com>
2020-05-11 18:35:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
   
   def kind
-    if @length1 + @length2 < @length3 ||@length1 + @length2 < @length3 || @length1 + @length2 < @length3
+    if @length1 + @length2 =< @length3 ||@length1 + @length3 =< @length2 || @length2 + @length3 =< @length1
     elsif @length1 == @length2 && @length2 == @length3
       return :equilateral
     elsif @length1 == @length2 || length2 == @length3
