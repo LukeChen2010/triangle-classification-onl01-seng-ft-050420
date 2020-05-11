357f802c9c442b0e4d4594ecc199ecb5aeba6463
Luke Chen <luke.chen.2010@gmail.com>
2020-05-11 18:37:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
   
   def kind
-    if @length1 + @length2 =< @length3 ||@length1 + @length3 =< @length2 || @length2 + @length3 =< @length1
+    if @length1 + @length2 ,= @length3 ||@length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
       raise TriangleError
     elsif @length1 < 0 || @length2 < 0 || @length3 < 0
       raise TriangleError
