8b82772bc390ccff79f75b453f67ea186b8e4198
Luke Chen <luke.chen.2010@gmail.com>
2020-05-11 18:36:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,9 @@ class Triangle
   
   def kind
     if @length1 + @length2 =< @length3 ||@length1 + @length3 =< @length2 || @length2 + @length3 =< @length1
-      
+      raise TriangleError
     elsif @length1 < 0 || @length2 < 0 || @length3 < 0
-      
+      raise TriangleError
     elsif @length1 == @length2 && @length2 == @length3
       return :equilateral
     elsif @length1 == @length2 || length2 == @length3
